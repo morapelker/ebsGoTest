5b5bc3c06ddd0d7030b3e988aa639b4e093d3e9f
morapelker <morapelker@gmail.com>
2021-11-10 21:45:08 +0200

    aa

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "asddd",
+			"message": "d",
 		})
+
 	})
 	r.GET("/mor", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"result": "ok"})
